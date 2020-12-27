467c3f6b84db5b61d611f1bc8c31c847cef0974c
Sóstenes <sostenesapollo25@gmail.com>
2020-12-27 18:01:46 -0300

    emit comment from FormTodo to  Comments

--- vuejs-cli/src/App.vue
+++ vuejs-cli/src/App.vue
@@ -9,6 +9,6 @@ export default {
   name: 'app',
   components: {
     Comments
-  }
+  } 
 }
 </script>
---
+++ vuejs-cli/src/components/FormTodo.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="form-todo form-group">
+        <p>
+            <input type="text" placeholder="Nome" class="form-control" v-model="name">
+        </p>
+        <p>
+            <textarea placeholder="Comentário" class="form-control" v-model="message"></textarea>
+        </p>
+        <button class="btn btn-info" v-on:click="addComment">Comentar</button>
+        <hr>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return { 
+                name: "",
+                message: ""
+            }
+        },
+        methods: {            
+            addComment() {
+                this.$emit("add-todo", {
+                    name: this.name,
+                    message: this.message
+                })
+
+                this.name = ""
+                this.message = ""
+            }
+        }
+    }
+</script>
